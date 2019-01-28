62120fad1ae935e90623e63e6aa1b4e98d02e5f6
Ed <ed-rod@users.noreply.github.com>
2019-01-28 11:52:52 +0000

    First commit of AOK application

---
+++ src/main/java/uk/co/eduardo/aok/Viewer.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) PRGX.
+ * All Rights Reserved.
+ */
+package uk.co.eduardo.aok;
+
+/**
+ * Main entry point.
+ *
+ * @author erodri02
+ */
+public class Viewer
+{
+   /**
+    * Main entry point.
+    *
+    * @param args ignored.
+    */
+   public static void main( final String[] args )
+   {
+      return;
+   }
+}
---
+++ src/test/java/uk/co/eduardo/aok/TestViewer.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) PRGX.
+ * All Rights Reserved.
+ */
+package uk.co.eduardo.aok;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Placeholder test class.
+ *
+ * @author erodri02
+ */
+public class TestViewer
+{
+   /**
+    * Placeholder. Does nothing.
+    */
+   @Test
+   public void testA()
+   {
+      Assert.assertTrue( true );
+   }
+}
